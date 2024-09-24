48451d170f9a761585028fc1756e9f9e1a5627ad
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-09-24 12:26:20 +0000

    transaction db setup

--- database/migrations/2024_09_24_122427_create_transfers_table.php
+++ database/migrations/2024_09_24_122427_create_transfers_table.php
@@ -4,18 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('transfers', function (Blueprint $table) {
             $table->id();
-            $table->float('amount', 2);
-            $table->tinyText('description');
-            $table->unsignedBigInteger('transactionable_id');
-            $table->string('transactionable_type');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('transfers');
     }
 };
